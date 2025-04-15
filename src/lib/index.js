b4af1d5c51a4bcd16409b52d6743241e05cfe03e
Matt Oestreich <matthewpoestreich@gmail.com>
2025-04-15 09:57:49 -0500

    update

---
+++ src/lib/index.js
@@ -0,0 +1,5 @@
+/**
+ * This is a shim for our cjs build.
+ */
+const PredictV8Randomness = require("./PredictV8Randomness").default;
+module.exports = PredictV8Randomness;
--- src/lib/index.ts
+++
@@ -1,5 +0,0 @@
-/**
- * This is a shim for our cjs build.
- */
-import PredictV8Randomness from "./PredictV8Randomness";
-export default PredictV8Randomness;
