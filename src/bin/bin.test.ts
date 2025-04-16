04a1bba8cc17ae5e0154dc980719d152ea63bc70
Matt Oestreich <matthewpoestreich@gmail.com>
2025-04-15 21:10:26 -0500

    fmt

--- src/bin/bin.test.ts
+++ src/bin/bin.test.ts
@@ -10,21 +10,9 @@ describe("Bin (CLI) : Dynamically Generated Sequence", () => {
 
 describe("Bin (CLI) : User Provided Sequence", () => {
   it("should predict the next five random numbers", async () => {
-    const USER_PROVIDED_SEQUENCE = [
-      0.34976166449621826,
-      0.6365063365192662,
-      0.1561153962043036,
-      0.856013937923775,
-      0.41010231718358936
-    ];
-  
-    const EXPECTED_NEXT_FIVE_NUMBERS = [
-      0.08905966816207322,
-      0.30334558505429565,
-      0.40213861949121776,
-      0.9458201944286209,
-      0.8958730034145181,
-    ];
+    const USER_PROVIDED_SEQUENCE = [0.34976166449621826, 0.6365063365192662, 0.1561153962043036, 0.856013937923775, 0.41010231718358936];
+
+    const EXPECTED_NEXT_FIVE_NUMBERS = [0.08905966816207322, 0.30334558505429565, 0.40213861949121776, 0.9458201944286209, 0.8958730034145181];
 
     const result = await runPredictor({ sequence: USER_PROVIDED_SEQUENCE, predictions: 5 });
     expect(result.mode).toBe("sequence");
@@ -34,8 +22,6 @@ describe("Bin (CLI) : User Provided Sequence", () => {
 
 describe("Bin (CLI) : yargs functionality", () => {
   it("should throw if neither seeds nor sequence is provided", async () => {
-    await expect(
-      runPredictor({ predictions: 5 } as any)
-    ).rejects.toThrow(/provide either --seeds or --sequence/);
+    await expect(runPredictor({ predictions: 5 } as any)).rejects.toThrow(/provide either --seeds or --sequence/);
   });
 });
--- src/lib/lib.test.ts
+++ src/lib/lib.test.ts
@@ -12,27 +12,15 @@ describe("Lib (Node) : Dynamically Generated Sequence", () => {
     const nextFive = await dynamicPredictor.predictFuture(5);
     const actualFive = Array.from({ length: 5 }, Math.random);
     expect(nextFive).toEqual(actualFive);
-  })
+  });
 });
 
 describe("Lib (Node) : User Provided Sequence", () => {
-  const USER_PROVIDED_SEQUENCE = [
-    0.34976166449621826,
-    0.6365063365192662,
-    0.1561153962043036,
-    0.856013937923775,
-    0.41010231718358936
-  ];
+  const USER_PROVIDED_SEQUENCE = [0.34976166449621826, 0.6365063365192662, 0.1561153962043036, 0.856013937923775, 0.41010231718358936];
 
   const EXPECTED_NEXT_NUMBER = 0.08905966816207322;
 
-  const EXPECTED_NEXT_FIVE_NUMBERS = [
-    0.30334558505429565,
-    0.40213861949121776,
-    0.9458201944286209,
-    0.8958730034145181,
-    0.3638471711087039
-  ];
+  const EXPECTED_NEXT_FIVE_NUMBERS = [0.30334558505429565, 0.40213861949121776, 0.9458201944286209, 0.8958730034145181, 0.3638471711087039];
 
   const providedPredictor = new Predictor(USER_PROVIDED_SEQUENCE);
 
