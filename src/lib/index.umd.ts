fbf955d897bad9fc19fe22da8110984721fef9e8
Matt Oestreich <matthewpoestreich@gmail.com>
2025-04-17 07:29:59 -0500

    Update index.umd.ts

--- src/lib/index.umd.ts
+++ src/lib/index.umd.ts
@@ -1,7 +1,10 @@
 /**
  * Shim so we can use this in the browser.
  */
-import ".";
+import Predictor from "./Predictor";
 import { Buffer } from "buffer";
+
+export default { Predictor };
+export { Predictor };
 
 window.Buffer = Buffer;
